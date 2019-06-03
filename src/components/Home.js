ae689f7c9e0bc78e9c5338b3ca9b99f87a7952f4
Jordan Ancheta <jordanancheta@gmail.com>
2019-06-03 10:31:46 -0700

    changes made to UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,100 @@
 import React from "react";
+import "../css/main.css";
+var bg = require("..//images/img-home.jpg")
 
 const Home = () => {
    return (
-      <div>
-         <section>
-            <div className="App App-content">
-               <h1>The Future of Bid Scoping</h1>
-               <p>Find Out More</p>
+
+   <div>
+      <div className="white-text-container background-image-container" style={ { backgroundImage: "url("+bg+")" } }>
+         <div className="container">
+            <div className="row">
+                  <div claclassNamess="col-md-6">
+                     <h1>Bidscope.io</h1>
+                     <h4>This is the future of bid scoping. A better way to mangage bids. <br/> Create more accurate scope sheets. <br/> Get more accurate bids. </h4>
+                     <a href="./download.html" title="" className="btn btn-lg btn-primary">Find Out More</a>
+                  </div>
             </div>
-         </section>
-         <section>
-            <div className="App section">
-               <div>
-                  <h3>What is Bidscope</h3>
+          </div>
+   </div>
+
+
+   <div className="section-container border-section-container">
+      <div className="container">
+         <div className="row">
+            <div className="col-md-12 section-container-spacer">
+               <div className="text-center">
+                  <h2>Bid Scoping Reimagined</h2>
+                  <p> Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing.<br /> Porta non 
+                        pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. </p>
                </div>
             </div>
-         </section>
+
+            <div className="col-md-4">
+               <div className="fa-container">
+                  <i className="fa fa-comment-o fa-3x" aria-hidden="true"></i>
+               </div>
+               <div className="text-center">
+                  <h3>Communicate</h3>
+               </div>
+               <div>
+                  <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
+                        non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
+                        Turpis egestas pretium aenean pharetra magna ac.
+                  </p>
+               </div>
+            </div>
+            <div className="col-md-4">
+               <div className="fa-container">
+                  <i className="fa fa-heart-o fa-3x" aria-hidden="true"></i>
+               </div>
+               <div className="text-center">
+                  <h3>Track</h3>
+               </div>
+               <div>
+                  <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
+                        non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
+                        Turpis egestas pretium aenean pharetra magna ac.
+                  </p>
+               </div>
+            </div>
+            <div className="col-md-4">
+               <div className="fa-container">
+                  <i className="fa fa-bell-o fa-3x" aria-hidden="true"></i>
+               </div>
+               <div className="text-center">
+                  <h3>Alerts</h3>
+               </div>
+               <div>
+                  <p>Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta
+                        non pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus.
+                        Turpis egestas pretium aenean pharetra magna ac. 
+                  </p>
+               </div>
+            </div>
+         </div>
       </div>
+   </div>
+
+
+   <div className="section-container background-color-container white-text-container">
+      <div className="container">
+        <div className="row">
+            <div className="col-xs-12">
+                <div className="text-center">
+                    <h2>Vivamus laoreet</h2>
+                    <p> Auctor augue mauris augue neque. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Porta non
+                        pulvinar neque laoreet. Viverra ipsum nunc aliquet bibendum. Iaculis urna id volutpat lacus. Turpis
+                        egestas pretium aenean pharetra magna ac. Id cursus metus aliquam eleifend mi. </p>
+                    <a href="./download.html" title="" className="btn btn-primary btn-lg">Check It Out</a>
+                </div>
+            </div>
+        </div>
+     </div>
+   </div>
+
+ </div>
+
    );
 };
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,44 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../css/main.css";
+import mashuptemplate from "..//images/mashuptemplate.svg"
 
 const Navigation = () => {
    return (
-      <header className="container sticky">
-         <div className="App">
-            <h1>BidScope.io</h1>
+
+   <header>
+      <nav className="navbar  navbar-fixed-top navbar-default">
+         <div className="container">
+            <div className="navbar-header">
+            <button type="button" className="navbar-toggle uarr collapsed" data-toggle="collapse" data-target="#navbar-collapse-uarr">
+               <span className="sr-only">Toggle navigation</span>
+               <span className="icon-bar"></span>
+               <span className="icon-bar"></span>
+               <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="./index.html" title="">
+               <img src={mashuptemplate} className="navbar-logo-img" alt="" />
+            </a>
+            </div>
+
+            <div className="collapse navbar-collapse" id="navbar-collapse-uarr">
+               <ul className="nav navbar-nav navbar-right">
+                  <li><NavLink to="/" className="home-link">Home</NavLink></li>
+                  {/* <li><a href="./about.html" title=""> About</a></li>
+                  <li><a href="./pricing.html" title=""> Pricing </a></li>
+                  <li><a href="./contact.html" title="">Contact</a></li> */}
+                  <li><NavLink to ="/pdf">Pdf Parse</NavLink></li>
+                  <li>
+                     <p>
+                     <button href="" className="btn btn-primary navbar-btn" title="">Sign In</button>
+                     </p>
+                  </li>
+               </ul>
+            </div>
          </div>
-         <nav className="nav">
-            <NavLink to="/" className="home-link">Home</NavLink>
-            <NavLink to="/pdf">Pdf Parse</NavLink>
-         </nav>
-      </header>
+      </nav>
+   </header>
+
    );
 };
 
